ef67ab5ea7c8cda3349f53fe5adf5ed24dadb0c3
SpartanSoftware <swoshswosh@proton.me>
2025-05-25 21:59:34 -0700

    Made the VM performant

--- obelisk.lua
+++ obelisk.lua
@@ -5,27 +5,30 @@ local obelisk = {}
 function obelisk.new()
     local obeliskoid = {
         stack = {},
+        stackTop = 0,
         bytecode = {},
         variables = {},
         functions = {},
-        flags = {equal = false, zero = false},
+        flags = { equal = false, zero = false },
         PC = 0,
     }
 
-    local function pop(self)
-        if #self.stack <= 0 then
-            self.stackUnderflow()
-        else
-            return table.remove(self.stack)
+    local function push(self, val)
+        self.stackTop = self.stackTop + 1
+        self.stack[self.stackTop] = val
+        if self.stackTop > 0x7FF then
+            self:stackOverflow()
         end
     end
 
-    local function push(self, val)
-        if #self.stack >= 0x7FF then
-            self.stackOverflow()
-        else
-            self.stack[#self.stack+1] = val
+    local function pop(self)
+        if self.stackTop == 0 then
+            self:stackUnderflow()
         end
+        local val = self.stack[self.stackTop]
+        self.stack[self.stackTop] = nil
+        self.stackTop = self.stackTop - 1
+        return val
     end
 
     local function pushEnv(self)
@@ -42,7 +45,7 @@ function obelisk.new()
                 return self.variables[i][name]
             end
         end
-        error("Undefined variable: "..name, 0)
+        error("Undefined variable: " .. name, 0)
     end
 
     local function setVar(self, name, value)
@@ -55,71 +58,116 @@ function obelisk.new()
         self.variables[#self.variables][name] = value
     end
 
-    function obeliskoid:run()
-        local instruction = self.bytecode[self.PC]
-        if not instruction then error("Obelisk could not find bytecode at "..self.PC, 0) end
+    local handlers = {
+        PUSH_IMMEDIATE = function(self, arg)
+            push(self, bit.band(arg, 0xFFFF))
+        end,
 
-        --print(instruction[1])
+        PUSH_VARIABLE = function(self, name)
+            push(self, getVar(self, name))
+        end,
 
-        if instruction[1] == "PUSH_IMMEDIATE" then
-            push(self, bit.band(instruction[2], 0xFFFF))
-        elseif instruction[1] == "PUSH_VARIABLE" then
-            local var = getVar(self, instruction[2])
-            if var == nil then error("Attempt to push a nil variable to stack", 0) end
-            push(self, var)
-        elseif instruction[1] == "ADD" then
-            local res = pop(self) + pop(self)
-            push(self, res)
-        elseif instruction[1] == "SUB" then
-            local res = pop(self) - pop(self)
-            push(self, res)
-        elseif instruction[1] == "MUL" then
-            local res = pop(self) * pop(self)
-            push(self, res)
-        elseif instruction[1] == "DIV" then
-            local res = pop(self) / pop(self)
-            push(self, res)
-        elseif instruction[1] == "CMP" then
-            local val = pop(self)
-            local val2 = pop(self)
-            self.flags.zero = val == 0
-            self.flags.equal = val == val2
-        elseif instruction[1] == "SET" then
-            setVar(self, instruction[2], pop(self))
-        elseif instruction[1] == "JUMP" then
+        ADD = function(self)
+            push(self, pop(self) + pop(self))
+        end,
+
+        SUB = function(self)
+            push(self, pop(self) - pop(self))
+        end,
+
+        MUL = function(self)
+            push(self, pop(self) * pop(self))
+        end,
+
+        DIV = function(self)
+            local b = pop(self)
+            local a = pop(self)
+            push(self, a / b)
+        end,
+
+        CMP = function(self)
+            local b = pop(self)
+            local a = pop(self)
+            self.flags.zero = b == 0
+            self.flags.equal = a == b
+        end,
+
+        SET = function(self, name)
+            setVar(self, name, pop(self))
+        end,
+
+        JUMP = function(self)
             self.PC = pop(self) - 1
-        elseif instruction[1] == "JUMP_IF_ZERO" then
-            local location = pop(self)
+        end,
+
+        JUMP_IF_ZERO = function(self)
+            local loc = pop(self)
             if self.flags.zero == true then
-                self.PC = location - 1
+                self.PC = loc - 1
             end
-        elseif instruction[1] == "JUMP_IF_NONZERO" then
-            local location = pop(self)
+        end,
+
+        JUMP_IF_NONZERO = function(self)
+            local loc = pop(self)
             if self.flags.zero == false then
-                self.PC = location - 1
+                self.PC = loc - 1
             end
-        elseif instruction[1] == "JUMP_IF_EQUAL" then
-            local location = pop(self)
+        end,
+
+        JUMP_IF_EQUAL = function(self)
+            local loc = pop(self)
             if self.flags.equal == true then
-                self.PC = location - 1
+                self.PC = loc - 1
             end
-        elseif instruction[1] == "JUMP_IF_NONEQUAL" then
-            local location = pop(self)
+        end,
+
+        JUMP_IF_NONEQUAL = function(self)
+            local loc = pop(self)
             if self.flags.equal == false then
-                self.PC = location - 1
+                self.PC = loc - 1
             end
-        elseif instruction[1] == "CALL" then
-            local location = pop(self)
+        end,
+
+        CALL = function(self)
+            local loc = pop(self)
             push(self, self.PC)
-            self.PC = location - 1
-        elseif instruction[1] == "PUSHENV" then
+            self.PC = loc - 1
+        end,
+
+        PUSHENV = function(self)
             pushEnv(self)
-        elseif instruction[1] == "POPENV" then
+        end,
+
+        POPENV = function(self)
             popEnv(self)
-        elseif instruction[1] == "HALT" then
-            return true
+        end,
+
+        HALT = function(self)
+            self.halted = true
         end
-        self.PC = bit.band(self.PC + 1, 0xFFFF)
+    }
+
+    function obeliskoid:run(cycles)
+        cycles = cycles or -1
+        local currentCycle = 0
+        self.halted = false
+        while not self.halted and cycles ~= currentCycle do
+            currentCycle = currentCycle + 1
+            local instruction = self.bytecode[self.PC]
+            if not instruction then
+                error("Obelisk could not find bytecode at: " .. self.PC, 0)
+            end
+
+            local opcode = instruction[1]
+            local handler = handlers[opcode]
+            if not handler then
+                error("Unknown opcode: " .. tostring(opcode), 0)
+            end
+
+            handler(self, instruction[2])
+
+            self.PC = bit.band(self.PC + 1, 0xFFFF)
+        end
     end
 
     function obeliskoid:quickBytecode(location, bytecode)
@@ -128,7 +176,7 @@ function obelisk.new()
         end
     end
 
-    function obeliskoid:stackOverflow() -- If you are in an environment requiring it, you can overwrite these
+    function obeliskoid:stackOverflow()
         error("Stack overflow", 0)
     end
 
